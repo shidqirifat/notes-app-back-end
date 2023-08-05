ddbe34a49bb2bd76d8d7283732e1a640fd38cfa7
Shidqi <rifatpangestu87@gmail.com>
2023-08-05 18:02:25 +0700

    feat: feature get user by id and add user

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const Hapi = require('@hapi/hapi');
 const NotesService = require('./services/postgres/NotesService');
+const UsersService = require('./services/postgres/UsersService');
+const users = require('./api/users');
 const notes = require('./api/notes');
 const NotesValidator = require('./validator/notes');
+const UsersValidator = require('./validator/users');
 require('dotenv').config();
 
 const init = async () => {
-  const noteService = new NotesService();
+  const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -17,13 +21,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: noteService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
